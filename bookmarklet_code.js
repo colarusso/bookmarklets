b0b353bc3752e50982487372c9803f8a1a6f7f42
David Colarusso <colarusso@users.noreply.github.com>
2020-09-20 14:54:09 -0400

    Update bookmarklet_code.js

--- bookmarklet_code.js
+++ bookmarklet_code.js
@@ -1,25 +1,3 @@
 var q = document.createElement('script');
 q.src = 'https://colarusso.github.io/boilerplate/scripts.js';
 document.body.appendChild(q);
-
-block_to_insert = document.createElement( 'div' );
-block_to_insert.id = "button";
-block_to_insert.innerHTML = `
-<select onchange="insertAtCursor(document.activeElement,collect(this.value))">
- <option value="">CHOOSE ONE</option>
- <option value="0">patience</option>
- <option value="1">office hours</option>
- <option value="2">press</option>
- <option value="3">introduction</option>
- <option value="4">thinking of me</option>
- <option value="5">head down</option>
- <option value="6">consideration</option>
- <option value="7">assistance</option>
- <option value="8">always</option>
-</select>
-`;
-block_to_insert.style.position = "fixed";
-block_to_insert.style.top = "10px";
-block_to_insert.style.left = "560px";
-container_block = document.body;
-container_block.appendChild( block_to_insert );
