2c882f8da14edd5d3dd7eec65e23c213e36c1caf
David Colarusso <colarusso@users.noreply.github.com>
2020-09-22 10:44:41 -0400

    Update and rename bookmarklet_code.js to boilerplate/bookmarklet.js

---
+++ boilerplate/bookmarklet.js
@@ -0,0 +1,3 @@
+var q = document.createElement('script');
+q.src = 'https://colarusso.github.io/bookmarklets/boilerplate/scripts.js';
+document.body.appendChild(q);
--- bookmarklet_code.js
+++
@@ -1,3 +0,0 @@
-var q = document.createElement('script');
-q.src = 'https://colarusso.github.io/boilerplate/scripts.js';
-document.body.appendChild(q);
