1b33025fdf96e991fe869b322b45115dfd2cbb73
David Colarusso <colarusso@users.noreply.github.com>
2021-03-01 12:38:56 -0500

    updated replies to include manel explination

--- boilerplate/scripts.js
+++ boilerplate/scripts.js
@@ -6,13 +6,13 @@ function collect(choice) {
 
  to = "" 
   
- if (choice >= 0 && choice<=11) {
+ if (choice >= 0 && choice<=12) {
   if (!document.querySelector('[aria-label="Reading Pane"]').innerHTML.match(/To:.*>\s?(.*)<\/li>/)){
    to = prompt("To?");
   } else {
    to = document.querySelector('[aria-label="Reading Pane"]').innerHTML.match(/To:.*>\s?(.*)<\/li>/)[1].split(" ")[0]
   }
- } else if (choice == 13) {
+ } else if (choice == 14) {
    window.open("https://doodle.com/create");          
  } else if (choice == 10000) {
    window.open("https://github.com/colarusso/bookmarklets/edit/master/boilerplate/scripts.js");
@@ -44,42 +44,45 @@ to + `,\n\nThank you for your patience. `
 ,to + `,\n\nThank you for the introduction. I'm moving you to BCC to avoid cluttering your inbox. \n\n` + introduced + `,\n\nIf you would like to jump on a call. You can find my availability and book a time here: https://calendly.com/dcolarusso/call\n\nI look forward to speaking soon. `
  
 // 5
-,to + `,\n\nThanks. I share this. `
+,to + `,\n\nGladly, with one condition. It's my personal policy not to sit on all-male panels. So, I'd love to participate as long as it's not a manel. `
 
 // 6
+,to + `,\n\nThanks. I share this. `
+
+// 7
 ,to + `,\n\nThank you for thinking of me. I look forward to giving this a look. `
    
-// 7
+// 8
 ,to + `,\n\nThank you for thinking of me. I hadn't seen this. `
   
-// 8
+// 9
 ,to + `,\n\nThank you. I'm head down on something right now, but I'll get back to you soon. `
 
-// 9
+// 10
 ,to + `,\n\nThat's a tough one. I'm not sure I have much to offer there. `
 
-// 10
+// 11
 ,to + `,\n\nThank you for reaching out. However, I'm not exactly sure what you're looking for. Could you be more specific about an ask, something more than just "looking to talk?" How exactly do you think I could help? `
   
-// 11
+// 12
 ,to + `,\n\nThank you for your interest in Learned Hands (https://learnedhands.law.stanford.edu/). The program allows you to track your hours within the app and requires no direct supervision. We hope that you find your work on the project fulfilling and that you'll share it with your colleagues.\n\nYou can see how we're putting your work to use here: https://spot.suffolklitlab.org/\n\nIf your pro bono office would like to learn more, please let me know. `
   
-// 12
+// 13
 ,`You can find my availability and book a time here: https://calendly.com/dcolarusso/call\n\nI look forward to speaking soon. `
   
-// 13
+// 14
 ,`To help schedule a time, I've set up a Doodle. I'll loop back after folks have a chance to mark their availability.\n\n `
    
-// 14
+// 15
 ,`Thank you for your time. `
 
-// 15
+// 16
 ,`Thank you for your time and consideration. ` 
   
-// 16
+// 17
 ,`Thank you for your time and assistance. `
 
-// 17
+// 18
 ,`As always, thank you for your time and assistance. `
 
 ];
@@ -119,21 +122,22 @@ block_to_insert.innerHTML = `
  <option value="2">Book Call</option>
  <option value="3">Book Press Call</option>
  <option value="4">Thx 4 Intro; Book Call.</option>
- <option value="5">Thx; I'll share.</option>
- <option value="6">Thx 4 thinking of me</option>
- <option value="7">Thx; Hadn't Seen</option>
- <option value="8">Head Down; Later</option>
- <option value="9">Tough One</option>
- <option value="10">Clarify Ask</option>
- <option value="11">LH Volunteer</option>
+ <option value="5">Thx; No Manals.</option>
+ <option value="6">Thx; I'll share.</option>
+ <option value="7">Thx 4 thinking of me</option>
+ <option value="8">Thx; Hadn't Seen</option>
+ <option value="9">Head Down; Later</option>
+ <option value="10">Tough One</option>
+ <option value="11">Clarify Ask</option>
+ <option value="12">LH Volunteer</option>
  <option disabled>──────────</option>
  <option disabled>STRING:</option>
- <option value="12">Book Call</option>
- <option value="13">Doodle</option>
- <option value="14">Thx 4 UR Time</option>
- <option value="15">Thx 4 UR Time &amp; Consideration</option>
- <option value="16">Thx 4 UR Time &amp; Assistance</option>
- <option value="17">As Always, Thx</option>
+ <option value="13">Book Call</option>
+ <option value="14">Doodle</option>
+ <option value="15">Thx 4 UR Time</option>
+ <option value="16">Thx 4 UR Time &amp; Consideration</option>
+ <option value="17">Thx 4 UR Time &amp; Assistance</option>
+ <option value="18">As Always, Thx</option>
  <option disabled>──────────</option>
  <option value="10000">Edit Bookmarklet</option>
  <option value="20000">Bookmarklet Maker</option>
@@ -142,6 +146,6 @@ block_to_insert.innerHTML = `
 `;
 block_to_insert.style.position = "fixed";
 block_to_insert.style.top = "15px";
-block_to_insert.style.right = "355px";
+block_to_insert.style.right = "400px";
 container_block = document.body;
 container_block.appendChild( block_to_insert );
